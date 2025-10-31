affb7a3e960f62362b9f6ea5d894aabd83cd3cf8
Khadeeja35 <khadeejabibi35@gmail.com>
2025-10-31 19:32:45 -0400

    FINAL

--- Lab 6.py
+++ Lab 6.py
@@ -6,7 +6,7 @@
 
 # Question 1: How does the population size vary by region?
 # Question 2: How does women represetation in parliament vary by subregion?
-# Question 3: Is there a association between GNI per capita and the number of physicians?
+# Question 3: Is there a association between gas emission per capita and life expectancy?
 
 # 2) 
 
@@ -20,26 +20,33 @@ df = pd.read_csv("wdi_wide.csv")
 
 df.info()
 
-# Answer: There are 10 empty values for the column "Physicians" and 0 empty values for the "Population" column.
+# Answer: There are 10 empty values for the column "Physicians" and 
+#         0 empty values for the "Population" column.
 
 # 4)
 
 df.nunique()
 print(df.nunique())
 
-# Answer: 
+# Answer: This command is used to find the number of unique values in each column.
+#         For example, when we run it, we can see that there is 5 different regions, 
+#         217 different countries, etc. in the data set.
 
 # 5)
 
 df.describe()
 print(df.describe())
 
-# Answer: The output of this function provide us the descriptive statistics of the dataset (mean, standard deviation, etc.)
+# Answer: The output of this function provide us the descriptive statistics 
+#         of the dataset (mean, standard deviation, etc.)
 
 # 6)
 
 df["GNI per capita"] = df["GNI"]/df["Population"]
 print(df["GNI per capita"].round())
+
+# Answer: The output of this function give us the GNI per person because 
+#         we have a lot of data we use the average of GNI per person.
 
 # 7)
 
@@ -48,7 +55,8 @@ print(df["GNI per capita"].round())
 country_count = df["Region"].value_counts()
 print(country_count)
 
-# Answer: There are 54 countries in Africa, 50 countries in Asia, 47 in Europe, 46 in Americas and 19 in Oceania. 
+# Answer: There are 54 countries in Africa, 50 countries in Asia, 47 in Europe, 
+#         46 in Americas and 19 in Oceania. 
 
   # b)
 
@@ -62,7 +70,7 @@ print(high_income_count)
 high_income_economy = pd.crosstab(df["Region"],df["High Income Economy"])
 print(high_income_economy)
 
-# There is 17 high income economy in Americas, 14 in Asia, 31 in Europe and 4 in Oceania
+# There is 17 high income economy in Americas, 14 in Asia, 31 in Europe and 4 in Oceania.
 
 # 9)
 
@@ -78,7 +86,7 @@ for i, row in df.iterrows():
         countries.append(row["Country Name"])
 print(countries)
 
-# Answer: There are 66 countries where women can expect to live for more than 80 years
+# Answer: There are 66 countries where women can expect to live for more than 80 years.
 
 
 #                                           PART 4
@@ -86,117 +94,183 @@ print(countries)
 # 1) 
 
 df["GNI per capita"] = df["GNI"] / df["Population"]
-sns.relplot(data = df, x= "Life expectancy, female", y = "GNI per capita", kind = "scatter", height = 5, aspect = 1.5)
+sns.relplot(data = df, 
+            x= "Life expectancy, female", 
+            y = "GNI per capita", 
+            kind = "scatter", 
+            height = 5, 
+            aspect = 1.5)
 plt.title ("Association between GNI per capita and Female Life Expectancy")
-plt.show()
 
 df["GNI per capita"] = df["GNI"] / df["Population"]
-sns.relplot(data = df, x= "Life expectancy, male", y = "GNI per capita", kind = "scatter", height = 5, aspect = 1.5)
+sns.relplot(data = df, 
+            x= "Life expectancy, male", 
+            y = "GNI per capita", 
+            kind = "scatter", 
+            height = 5, 
+            aspect = 1.5)
 plt.title ("Association between GNI per capita and Male Life Expectancy")
-plt.show()
 
-# Answer: Yes, there is a positive association between GNI per capita and life expectancy for both men and women. 
-#         From the graph we can see that as GNI per capita increases, life expectancy also tend to increase. 
-#         This means that richer countries have higher life expectancy because they have better healthcare systems, higher education levels, etc.
-#         Also, the female life expectancy tends to be slightly higher than male life expectancy.
+# Answer: Yes, there is a positive association between GNI per capita and 
+#         life expectancy for both men and women. From the graph we can see that 
+#         as GNI per capita increases, life expectancy also tend to increase. 
+#         This means that richer countries have higher life expectancy because 
+#         they have better healthcare systems, higher education levels, etc.
+#         Also, the female life expectancy tends to be slightly higher than 
+#         male life expectancy.
 
 # 2)
 
 df["GNI per capita"] = df["GNI"] / df["Population"]
-sns.relplot(data = df, x= "Life expectancy, female", y = "GNI per capita", hue = "Region", kind = "scatter", height = 5, aspect = 1.5)
+sns.relplot(data = df, 
+            x= "Life expectancy, female", 
+            y = "GNI per capita", 
+            hue = "Region", 
+            kind = "scatter", 
+            height = 5, 
+            aspect = 1.5)
 plt.title("GNI per capita vs Female Life Expectancy by Region")
-plt.show()
 
 df["GNI per capita"] = df["GNI"] / df["Population"]
-sns.relplot(data = df, x= "Life expectancy, male", y = "GNI per capita", hue = "Region", kind = "scatter", height = 5, aspect = 1.5)
+sns.relplot(data = df, 
+            x= "Life expectancy, male", 
+            y = "GNI per capita", 
+            hue = "Region", 
+            kind = "scatter", 
+            height = 5, 
+            aspect = 1.5)
 plt.title("GNI per capita vs Male Life Expectancy by Region")
-plt.show()
 
-# Answer: Yes, the association between GNI per capita and life expectancy does vary by region for both men and women.
-#         From the previous question, we saw that there is a positive relationship between GNI per capita and life expectancy.
-#         However, this relationship differ by regions:
+# Answer: Yes, the association between GNI per capita and life expectancy does 
+#         vary by region for both men and women. From the previous question, 
+#         we saw that there is a positive relationship between GNI per capita 
+#         and life expectancy. However, this relationship differ by regions:
 #         In Africa, there is low GNI per capita and low life expectancy.
 #         In Europe, there is high GNI per capita and high life expectancy.
-#         In Oceania, Asia and Americas, it is mainly moderate for both GNI per capita and life expectancy
+#         In Oceania, Asia and Americas, it is mainly moderate for both GNI 
+#         per capita and life expectancy
 
 # 3)
 
-
+df["GNI per capita"] = df["GNI"] / df["Population"]
+sns.relplot(data = df,
+            x= "Life expectancy, female",
+            y = "GNI per capita", 
+            hue = "Region",
+            kind = "scatter",
+            height = 5, 
+            aspect = 1.5)
+plt.title("GNI per capita vs Female Life Expectancy by Region")
 
 # 4)
 
-
+df["GNI per capita"] = df["GNI"] / df["Population"]
+sns.lmplot(data = df,
+            x= "Life expectancy, female",
+            y = "GNI per capita", 
+            hue = "Region",
+            height = 5, 
+            aspect = 1.5)
+plt.title("GNI per capita vs Female Life Expectancy by Region")
 
 # 5)
 
 # Comparing Female Tertiary Education and Female Life Expectancy by region
 
-sns.relplot(data = df, x= "Life expectancy, female", y = "Tertiary education, female", hue = "Region", kind = "scatter", height = 5, aspect = 1.5)
+sns.relplot(data = df, 
+            x= "Life expectancy, female", 
+            y = "Tertiary education, female", 
+            kind = "scatter", 
+            col = "Region",
+            height = 5, 
+            aspect = 1.5)
 plt.title("Female Tertiary education vs Female Life Expectancy by region")
-plt.show()
 
 # Comparing Male Tertiary Education and Male Life Expectancy by region
 
-sns.relplot(data = df, x= "Life expectancy, male", y = "Tertiary education, male", hue = "Region", kind = "scatter", height = 5, aspect = 1.5)
+sns.relplot(data = df, 
+            x= "Life expectancy, male", 
+            y = "Tertiary education, male", 
+            kind = "scatter", 
+            col = "Region",
+            height = 5, 
+            aspect = 1.5)
 plt.title("Male Tertiary education vs Male Life Expectancy by region")
-plt.show()
 
-# Comparing Internet Use and Female Life Expectancy by region
+# Comparing Gas Emission per capita and Female Life Expectancy by region
 
-sns.relplot(data = df, x= "Life expectancy, female", y = "Internet use", hue = "Region", kind = "scatter", height = 5, aspect = 1.5)
-plt.title("Internet use vs Female Life Expectancy by region")
-plt.show()
+df["emission per capita"] = df["Greenhouse gas emissions"] / df["Population"]
+sns.relplot(data = df, 
+            x= "Life expectancy, female", 
+            y = "emission per capita", 
+            kind = "scatter",
+            col = "Region",
+            height = 5, 
+            aspect = 1.5)
+plt.title("Emission per capita vs Female Life Expectancy by region")
 
-# Comparing Internet Use and Male Life Expectancy by region
+# Comparing Gas Emissions per capita and Male Life Expectancy by region
 
-sns.relplot(data = df, x= "Life expectancy, male", y = "Internet use", hue = "Region", kind = "scatter", height = 5, aspect = 1.5)
-plt.title("Internet use vs Male Life Expectancy by region")
-plt.show()
+df["emission per capita"] = df["Greenhouse gas emissions"] / df["Population"]
+sns.relplot(data = df, 
+            x= "Life expectancy, male", 
+            y = "emission per capita",  
+            kind = "scatter",
+            col = "Region",
+            height = 5, 
+            aspect = 1.5)
+plt.title("Emission per capita vs Male Life Expectancy by region")
 
-# Answer: By exploring some female life expectancy relationship with some of the other numerical feature, 
-#         we see that for male life expectancy, these relationshsip are the same for both.
+# Answer: By exploring some female life expectancy relationship with some of 
+#         the other numerical feature, we see that for male life expectancy, 
+#         these relationshsip are the same for both.
 
 # Question 1: How does internet use vary across regions?
 
-sns.barplot(data = df, x= "Region", y = "Internet use",)
+sns.barplot(data = df, 
+            x= "Region", 
+            y = "Internet use",)
 plt.title("Internet use by Region")
-plt.show()
 
 # Answer: Europe has the most internet users compared to Africa which has the less internet users.
 
 # Question 2: Which subregion has the most GNI per capita?
 
 df["GNI per capita"] = df["GNI"] / df["Population"]
-sns.barplot(data = df, x= "Subregion", y = "GNI per capita")
+sns.barplot(data = df, 
+            x= "Subregion", 
+            y = "GNI per capita")
 plt.title("GNI per capita by Subregion")
 plt.xticks(rotation=90)
-plt.show()
 
 # Answer: Western Europe and Northern America have the most GNI per capita.
 
 # Question 3: How does women’s representation in parliament vary by region?
 
-sns.barplot(data = df, x = "Region", y = "Women in national parliament")
+sns.barplot(data = df, 
+            x = "Region", 
+            y = "Women in national parliament")
 plt.title("Women in National Parliament by Region")
 plt.xticks(rotation=90)
-plt.show()
 
 # Answer: Regions like Europe and Americas have more women in parliament comapred to 
-#         Oceania who have few women int heir parliament.
+#         Oceania who have few women in their parliament.
 
 # Question 4: Which region has the most international tourism?
 
-sns.barplot(data = df, x= "Region", y = "International tourism")
+sns.barplot(data = df, 
+            x= "Region", 
+            y = "International tourism")
 plt.title("International Tourism by Region")
-plt.show()
 
 # Answer: Europe has the highest level of international tourism.
 
 # Question 5 : How does the population size vary among regions?
     
-sns.barplot(data = df, x= "Region", y = "Population")
+sns.barplot(data = df, 
+            x= "Region", 
+            y = "Population")
 plt.title("Population by Region")
-plt.show()
 
 # Answer: Asia is the region with the most population while Oceania with the less population.
 
@@ -205,9 +279,13 @@ plt.show()
   # a)
 
 df["emission per capita"] = df["Greenhouse gas emissions"] / df["Population"]
-sns.relplot(data = df, x= "Internet use", y = "emission per capita", kind = "scatter", height = 5, aspect = 1.5)
+sns.relplot(data = df, 
+            x= "Internet use", 
+            y = "emission per capita", 
+            kind = "scatter", 
+            height = 5, 
+            aspect = 1.5)
 plt.title("Emission per capita vs Internet Use")
-plt.show()
 
 # Answer: There isn't a clear association between the internet use and emission per capita. 
 
@@ -217,7 +295,8 @@ df["emission per capita"] = df["Greenhouse gas emissions"] / df["Population"]
 high_emissions_countries = df[df["emission per capita"] > 0.03]
 print(high_emissions_countries)
 
-# Answer: The countries that have high gas emissions are Brunei Darussalam and Qatar both situated in the same region, Asia.
+# Answer: The countries that have high gas emissions are Brunei Darussalam and 
+#         Qatar both situated in the same region, Asia.
 
   # c)
 
@@ -228,6 +307,7 @@ print(high_emissions_countries)
   # d)
 
 # Answer: From PART 3 question 7) b), we found that there is 67 high income economies 
-#         and from PART 4 question 6) b), we found that there is only 2 countries that have high gas emissions. 
-#         Thus, no not all high income economies have high gas emissions. 
-#         However, both countries that have high gas emissions also have high income economies.
+#         and from PART 4 question 6) b), we found that there is only 2 countries 
+#         that have high gas emissions. Thus, no not all high income economies 
+#         have high gas emissions. However, both countries that have high gas emissions 
+#         also have high income economies.
